30e6fc5c51ad0232ed32961de17efa2f111027a1
Austin Rooks <Austionian@users.noreply.github.com>
2023-01-30 20:20:10 -0600

    day 8 refactor

--- src/bin/day8.rs
+++ src/bin/day8.rs
@@ -8,8 +8,6 @@ struct Digs {
     output: Vec<String>,
 }
 
-const SUMS: [u32; 10] = [42, 17, 34, 39, 30, 37, 41, 25, 49, 45];
-
 impl FromStr for Digs {
     type Err = anyhow::Error;
 
@@ -21,6 +19,40 @@ impl FromStr for Digs {
         })
     }
 }
+
+impl Digs {
+    fn collect_values(&self, map: HashMap<char, u32>) -> Result<Vec<u32>> {
+        Ok(self
+            .output
+            .iter()
+            .map(|j| {
+                j.chars().fold(0, |acc, c| {
+                    map.get(&c).expect("value not in map").clone() + acc
+                })
+            })
+            .collect::<Vec<u32>>())
+    }
+
+    fn freq_analysis(&self) -> Result<u32> {
+        let mut map = HashMap::new();
+        self.input.iter().for_each(|i| {
+            i.chars().for_each(|c| {
+                map.entry(c).and_modify(|cc| *cc += 1).or_insert(1);
+            })
+        });
+
+        let value = self
+            .collect_values(map)?
+            .iter()
+            .map(|num| SUMS.iter().position(|&i| i == *num).unwrap().to_string())
+            .collect::<String>()
+            .parse::<u32>()?;
+
+        Ok(value)
+    }
+}
+
+const SUMS: [u32; 10] = [42, 17, 34, 39, 30, 37, 41, 25, 49, 45];
 
 const LENS: [u32; 4] = [2, 4, 3, 7];
 
@@ -46,38 +78,30 @@ fn find_easy() -> Result<u32> {
 fn find_hard() -> Result<u32> {
     let values = read_one_at_a_time::<Digs>("./data/8.input")?;
 
-    Ok(values
-        .iter()
-        .map(|v| {
-            let mut map = HashMap::new();
-            for i in v.input.iter() {
-                i.chars().for_each(|c| {
-                    map.entry(c).and_modify(|cc| *cc += 1).or_insert(1);
-                });
-            }
-
-            let mut nums = vec![];
-
-            v.output.iter().for_each(|j| {
-                nums.push(
-                    j.chars()
-                        .map(|c| map.get(&c).expect("value not in map").clone())
-                        .sum::<u32>(),
-                )
-            });
-
-            let value = nums
-                .iter()
-                .map(|num| SUMS.iter().position(|&i| i == *num).unwrap().to_string())
-                .collect::<String>();
-
-            value.parse::<u32>().unwrap()
-        })
-        .sum::<u32>())
+    Ok(values.iter().fold(0, |acc, v| {
+        v.freq_analysis().expect("freq analysis failed") + acc
+    }))
 }
 
 fn main() -> Result<()> {
     println!("Part One: {}", find_easy()?);
     println!("Part Two: {}", find_hard()?);
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn part_one() {
+        let answer = find_easy().unwrap();
+        assert_eq!(answer, 397);
+    }
+
+    #[test]
+    fn part_two() {
+        let answer = find_hard().unwrap();
+        assert_eq!(answer, 1027422);
+    }
 }
